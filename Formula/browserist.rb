314ec15e0ab42ff70a5726a7b42966316996444d
Jakob Bagterp <jakob_bagterp@hotmail.com>
2022-02-09 02:09:26 +0100

    Attempt to fix issue with Homebrew formula: "ERROR: No matching distribution found for selenium>=4.1.0"

--- Formula/browserist.rb
+++ Formula/browserist.rb
@@ -3,8 +3,8 @@ class Browserist < Formula
 
   desc "Extension for the Selenium web driver that makes browser automation even easier"
   homepage "https://github.com/jakob-bagterp/browserist"
-  url "https://github.com/jakob-bagterp/browserist/releases/download/v0.1.2/browserist-0.1.2.tar.gz"
-  sha256 "0e5065b949c3269928cd4cf1646fb51e20ef8794013ed172e7cf04691b58438e"
+  url "https://github.com/jakob-bagterp/browserist/releases/download/v0.1.2/browserist-0.1.2.post1.tar.gz"
+  sha256 "03f89419a0b636c19c979f7bd3ae85043e5da452cabb955e60cd1d576b121db3"
   license "Apache-2.0"
 
   depends_on "python@3.10"
