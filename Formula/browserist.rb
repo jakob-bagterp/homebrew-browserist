19b1aa12617e0de27d55821281e430991ced4db0
Jakob Bagterp <jakob_bagterp@hotmail.com>
2023-11-08 00:23:23 +0100

    Fix URL to version 1.5.3 download.
    (cherry picked from commit 81394abff88cd152982e5f6d2b91d70cbbeb0900)

--- Formula/browserist.rb
+++ Formula/browserist.rb
@@ -3,7 +3,7 @@ class Browserist < Formula
 
   desc "Extension for the Selenium web driver that makes browser automation even easier"
   homepage "https://github.com/jakob-bagterp/browserist"
-  url "hhttps://github.com/jakob-bagterp/browserist/releases/download/v1.5.3/browserist-1.5.3.tar.gz"
+  url "https://github.com/jakob-bagterp/browserist/releases/download/v1.5.3/browserist-1.5.3.tar.gz"
   sha256 "2a47fd2838af16ea257efe8e0e708c0d2c438e5184a0889ae451f358cf12fce3"
   license "Apache-2.0"
 
