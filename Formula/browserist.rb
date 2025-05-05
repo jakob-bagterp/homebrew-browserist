5229910e4b527c9616bd8258af7848156a8186d2
Jakob Bagterp <25110864+jakob-bagterp@users.noreply.github.com>
2025-05-05 22:44:21 +0200

    Updated formula to version 1.7.6

--- Formula/browserist.rb
+++ Formula/browserist.rb
@@ -3,8 +3,8 @@ class Browserist < Formula
 
   desc "Extension for the Selenium web driver that makes browser automation even easier"
   homepage "https://github.com/jakob-bagterp/browserist"
-  url "https://github.com/jakob-bagterp/browserist/releases/download/v1.7.5/browserist-1.7.5.tar.gz"
-  sha256 "5cafacb55cb875ddacd8e42df9c9bc37d7bae6813f48b1f009c41a8849d48f43"
+  url "https://github.com/jakob-bagterp/browserist/releases/download/v1.7.6/browserist-1.7.6.tar.gz"
+  sha256 "0171b95fcaf2d6ab4b204a63e2daff663e94f071f9ad7a1d5adec6e3d629f729"
   license "Apache-2.0"
 
   depends_on "python@3.11"
